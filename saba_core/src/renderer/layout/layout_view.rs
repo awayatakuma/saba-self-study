fc082bd93bbc3bd601cd3e6d1723ae0c483da7ec
awayatakuma <takuma.awaya@gmail.com>
2024-11-27 16:46:51 +0900

    add functions for gui in ch5

--- saba_core/src/renderer/layout/layout_view.rs
+++ saba_core/src/renderer/layout/layout_view.rs
@@ -1,12 +1,16 @@
 use alloc::rc::Rc;
+use alloc::vec::Vec;
+use core::alloc::Layout;
 use core::cell::RefCell;
 
 use crate::constants::CONTENT_AREA_WIDTH;
+use crate::display_item::DisplayItem;
 use crate::renderer::dom::api::get_target_element_node;
 use crate::renderer::dom::node::Node;
 use crate::renderer::layout::layout_object::LayoutObject;
 use crate::renderer::{css::cssom::StyleSheet, dom::node::ElementKind};
 
+use super::computed_style::DisplayType;
 use super::layout_object::{create_layout_object, LayoutObjectKind, LayoutPoint, LayoutSize};
 
 #[derive(Debug, Clone)]
@@ -92,6 +96,29 @@ impl LayoutView {
 
             n.borrow_mut().compute_size(parent_size);
         }
+    }
+
+    fn paint_node(node: &Option<Rc<RefCell<LayoutObject>>>, display_items: &mut Vec<DisplayItem>) {
+        match node {
+            Some(n) => {
+                display_items.extend(n.borrow_mut().paint());
+
+                let first_child = n.borrow().first_child();
+                Self::paint_node(&first_child, display_items);
+
+                let next_sibling = n.borrow().next_sibling();
+                Self::paint_node(&next_sibling, display_items);
+            }
+            None => (),
+        }
+    }
+
+    pub fn paint(&self) -> Vec<DisplayItem> {
+        let mut display_items = Vec::new();
+
+        Self::paint_node(&self.root, &mut display_items);
+
+        display_items
     }
 }
 
